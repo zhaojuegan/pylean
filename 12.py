915bb473eda627c1ce78c3a3e634bdd2745436d2
zhaojuegan <zhaojuegan@163.com>
2020-05-13 09:11:00 +0800

    Initial commit

---
+++ 12.py
@@ -0,0 +1,34 @@
+# a=201905018
+# nian=str(a//100000)
+# yue=str(a//1000%20190)
+# ri=str(a%100)
+# print(nian+"年"+yue+"月"+ri+"日")
+#
+# a=[8,8.8,True,8j]
+# print(a[2])
+# print(a[0:4][2])
+# print(a[1:3][1])
+# print(a[2::-1][0])
+# print(a[2::-3][0])
+# print("####"*29)
+# print(a[0:])
+# print(a[0:-1])
+# print(a[2:-1])
+# print(a[0:-2])
+# print(a[:-2])
+# print(a[:-2])
+# print(a[:-2])
+# print(a[:-2])
+# z=(1,34)
+# print(type(z))
+
+# townee=[{'海底王国':['小美人鱼''海之王''小美人鱼的祖母''五位姐姐','邻国公主']},'丑小鸭','坚定的锡兵','睡美人','青蛙王子',[{'主角':'小红帽','配角1':'外婆','配角2':'猎人'},{'反面角色':'狼'}]]
+# #a=townee[5][1]
+# print(townee[5][1]['反面角色'])
+li=[1,2,3,4]
+li[2]=9
+print(li)
+li.append(5)
+li.append(7)
+print(li)
+#print(dir(li))
---
+++ huahua.py
@@ -0,0 +1,13 @@
+#-*-coding-*-
+import requests
+import time
+class tz(object):
+    def _int_(self):
+        self.hexder={
+            "terminxlType":"4",
+            "User-xgent": "Mozillx/5.0 (Windows NT 10.0; WOW64) xppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/74.0.3729.131 Sxfxri/537.36",
+            "Host":"open.shiguxngkey.com",
+            "": "",
+            "": "",
+        }
